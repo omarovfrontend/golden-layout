8c77683a1f5eb85e294e968cadcdf89a07a81a45
Shamil Omarov <s_omarov05@mail.ru>
2020-12-31 22:54:25 +0300

    рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 $(function() {
-    function toggleHeaderClass(scrollOffset) {
+    // переменные
+    const dataScrolls = $('[data-scroll]');
+    const navToggleBtn = $('#nav-toggle');
+    const nav = $('#nav');
+
+    // событие скролла окна
+    $(window).on('scroll', function() {
+        // получаем отступ сверху
         const header = $('#header');
         const introHeight = $('#intro').innerHeight();
+        const scrollOffset = $(this).scrollTop();
 
         if(scrollOffset >= introHeight) {
             header.addClass('header_fixed');
-        }
-        else {
+        } else {
             header.removeClass('header_fixed');
         }
-    }
-
-    $(window).on('scroll', function() {
-        const scrollOffset = $(this).scrollTop();
-
-        toggleHeaderClass(scrollOffset);
     });
 
-    /* Smooth scroll */
-    $('[data-scroll]').on('click', function(event) {
+    // нажатие на пункты меню с атрибутом data-scroll
+    dataScrolls.on('click', function(event) {
         event.preventDefault();
 
         const $this = $(this);
         const blockId = $this.data('scroll');
         const blockOffset = $(blockId).offset().top;
 
-        $('[data-scroll]').removeClass('nav__link_active');
+        dataScrolls.removeClass('nav__link_active');
         $this.addClass('nav__link_active');
 
         $('html, body').animate({
@@ -33,12 +34,12 @@ $(function() {
         }, 300);
     });
 
-    /* Menu nav toggle */
-    $('#nav-toggle').on('click', function(event) {
+    // нажатие на переключатель меню в адаптиве
+    navToggleBtn.on('click', function(event) {
         event.preventDefault();
 
-        $(this).toggleClass('nav-toggle_active');
-        $('#nav').toggleClass('nav_active');
+        navToggleBtn.toggleClass('nav-toggle_active');
+        nav.toggleClass('nav_active');
     });
 
 });
